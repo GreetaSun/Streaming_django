5b82e03667dd2eac599372ec5c284216bb014277
xuyang <1396449715@qq.com>
2020-06-13 15:32:33 +0800

    '这是一个初始化的版本'

---
+++ streaming/app/client/__init__.py
@@ -0,0 +1,3 @@
+#encoding: utf-8
+#__author:  'xuyang'
+#date:  '2020/6/13 15:07'  
---
+++ streaming/app/client/urls.py
@@ -0,0 +1,8 @@
+#encoding: utf-8
+#__author:  'xuyang'
+#date:  '2020/6/13 15:16'
+from django.urls import path
+
+urlpatterns = [
+
+]
